8a717fe28f091c91f9da467f8daefee623e3f0a3
mioe <mioe.boss@gmail.com>
2021-05-20 11:20:17 +0300

    add types for nodejs

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import VueI18n from '@intlify/vite-plugin-vue-i18n'
 import ViteComponents from 'vite-plugin-components'
 import ViteIcons, { ViteIconsResolver } from 'vite-plugin-icons'
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   console.log('🦕 vite.config.ts/defineConfig', command, mode)
